cc6b62df3f4bf53789699c004b6ae42dc242cde9
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-09-08 19:38:47 +0530

    Update kcirllor.py

--- kcirllor.py
+++ kcirllor.py
@@ -1,3 +1,3 @@
-#Just wanted to say that I don't own the video.
+# Just wanted to say that I don't own the video.
 import webbrowser
 webbrowser.open_new_tab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
